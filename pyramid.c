c88ba04df22e00e5fb16c14685132046d9ffe813
Özgür Bilgen <45699938+Aesgiyr@users.noreply.github.com>
2023-03-25 01:50:29 +0300

    Pyramid star

---
+++ pyramid.c
@@ -0,0 +1,28 @@
+//
+//  pyramid.c
+//  Pyramid star
+//
+//  Created by Özgür Bilgen on 25.03.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    int i,j,k,line;
+    
+    printf("Enter number of lines: ");
+    scanf("%d",&line);
+    
+    for (i = 1; i<line+1; i++) {
+        for (j=i; j<line; j++) {
+            printf(" ");
+        }
+        for (k=0; k<2*i-i; k++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
